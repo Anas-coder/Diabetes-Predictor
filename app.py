d027538eb277743af3f7923eb29c892b7366bb0d
Md. Anas Ansari <54945647+Anas-coder@users.noreply.github.com>
2020-09-04 16:43:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    return render_template('index.html', prediction_text='my-pred.....Thanks for trying'.format(prediction))
+    return render_template('index.html', prediction_text=.format(prediction))
 
 
 if __name__ == "__main__":
