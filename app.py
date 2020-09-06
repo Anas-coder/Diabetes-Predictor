167ac29ad607b0be94d7fc3af9b85f64c000d4f6
Md. Anas Ansari <54945647+Anas-coder@users.noreply.github.com>
2020-09-06 13:27:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,7 @@ def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features):
-        if prediction == 0:
-            print('No')
-        else:
-            print('Yes')
+        
 
     return render_template('index.html', prediction_text='{}.....Thanks for trying'.format(prediction))
 
